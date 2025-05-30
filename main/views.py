346aa1142f2f953f579b3f66a569bbb52fee34b5
Vincent Alexander Haris <vincentalexx03@gmail.com>
2025-05-30 15:51:31 +0700

    predict data

--- main/views.py
+++ main/views.py
@@ -2,7 +2,23 @@ from django.shortcuts import render, HttpResponse
 from joblib import load
 
 logistic_regression_model = load('./models/logistic_regression_model.pkl')
-scaler = load('./scaler/scaler.pkl')
+decision_tree_model = load('./models/decision_tree_model.pkl')
+random_forest_model = load('./models/random_forest_model.pkl')
+adaboost_model = load('./models/adaboost_model.pkl')
+adaboost_model = load('./models/xgboost_model.pkl')
+xgboost_model = load('./models/xgboost_model.pkl')
+catboost_model = load('./models/catboost_model.pkl')
+linear_svm_model = load('./models/linear_svm_model.pkl')
+naive_bayes_model = load('./models/naive_bayes_model.pkl')
+
+lr_scaler = load('./scaler/logistic_regression_scaler.pkl')
+dt_scaler = load('./scaler/decision_tree_scaler.pkl')
+rf_scaler = load('./scaler/random_forest_scaler.pkl')
+ada_scaler = load('./scaler/adaboost_scaler.pkl')
+xg_scaler = load('./scaler/xgboost_scaler.pkl')
+# cat_scaler = load('./scaler/catboost_scaler.pkl')
+svm_scaler = load('./scaler/linear_svm_scaler.pkl')
+nb_scaler = load('./scaler/naive_bayes_scaler.pkl')
 
 # Create your views here.
 def main(request):
@@ -21,12 +37,53 @@ def main(request):
 
         data = [[float(tv_subscriber), float(movie_subscriber), float(subscription_age), float(bill_avg), float(remaining_contract), float(service_failure_count), float(download_avg), float(upload_avg), float(download_over_limit), float(contract)]]
 
-        input_scaled = scaler.transform(data)
-
         if selected_model == 'logistic_regression':
+            scaler = lr_scaler
+            input_scaled = scaler.transform(data)
             predict = logistic_regression_model.predict(input_scaled)
             selected_model = 'Logistic Regression'
 
+        elif selected_model == 'decision_tree':
+            scaler = dt_scaler
+            input_scaled = scaler.transform(data)
+            predict = decision_tree_model.predict(input_scaled)
+            selected_model = 'Decision Tree'
+
+        elif selected_model == 'random_forest':
+            scaler = rf_scaler
+            input_scaled = scaler.transform(data)
+            predict = random_forest_model.predict(input_scaled)
+            selected_model = 'Random Forest'
+
+        elif selected_model == 'adaboost':
+            scaler = ada_scaler
+            input_scaled = scaler.transform(data)
+            predict = adaboost_model.predict(input_scaled)
+            selected_model = 'AdaBoost'
+
+        elif selected_model == 'xgboost':
+            scaler = xg_scaler
+            input_scaled = scaler.transform(data)
+            predict = xgboost_model.predict(input_scaled)
+            selected_model = 'XGBoost'
+
+        elif selected_model == 'catboost':
+            # scaler = cat_scaler
+            # input_scaled = scaler.transform(data)
+            predict = catboost_model.predict(data)
+            selected_model = 'CatBoost'
+
+        elif selected_model == 'linear_svm':
+            scaler = svm_scaler
+            input_scaled = scaler.transform(data)
+            predict = linear_svm_model.predict(input_scaled)
+            selected_model = 'Linear SVM'
+            
+        elif selected_model == 'naive_bayes':
+            scaler = nb_scaler
+            input_scaled = scaler.transform(data)
+            predict = naive_bayes_model.predict(input_scaled)
+            selected_model = 'Naive Bayes'
 
         if predict == 1:
             predict = "Customer will churn"
@@ -34,7 +91,6 @@ def main(request):
             predict = "Customer will not churn"
 
         return render(request, 'main.html', {'result' : predict, 'selected_model': selected_model})
-
-        # return render(request, 'main.html', {'result': result, 'tv_subscriber': tv_subscriber, 'movie_subscriber': movie_subscriber, 'subscription_age': subscription_age, 'bill_avg': bill_avg, 'remaining_contract': remaining_contract, 'service_failure_count': service_failure_count, 'download_avg': download_avg, 'upload_avg': upload_avg, 'download_over_limit': download_over_limit, 'contract': contract})
+    
     else:
         return render(request, 'main.html')
