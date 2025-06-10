dd2b0d6f7e3b1f7c684765792aeb5483289d7533
Vincent Alexander Haris <vincentalexx03@gmail.com>
2025-06-10 23:38:20 +0700

    update layout

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse
 from joblib import load
 from .models import Customer
+import locale
+
+locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
 
 logistic_regression_model = load('./models/logistic_regression_model.pkl')
 decision_tree_model = load('./models/decision_tree_model.pkl')
@@ -25,18 +28,27 @@ nb_scaler = load('./scaler/naive_bayes_scaler.pkl')
 def new_data(request):
     if request.method == 'POST':
         selected_model = request.POST['select_model']
-        tv_subscriber = request.POST['tv_subscriber']
-        movie_subscriber = request.POST['movie_subscriber']
-        subscription_age = request.POST['subscription_age']
-        bill_avg = request.POST['bill_avg']
-        remaining_contract = request.POST['remaining_contract']
-        service_failure_count = request.POST['service_failure_count']
-        download_avg = request.POST['download_avg']
-        upload_avg = request.POST['upload_avg']
-        download_over_limit = request.POST['download_over_limit']
-        contract = request.POST['contract']
+        customer_data = {
+            'is_tv_subscriber': request.POST['tv_subscriber'],
+            'is_movie_package_subscriber': request.POST['movie_subscriber'],
+            'subscription_age': request.POST['subscription_age'],
+            'bill_avg': request.POST['bill_avg'],
+            'remaining_contract': request.POST['remaining_contract'],
+            'service_failure_count': request.POST['service_failure_count'],
+            'download_avg': request.POST['download_avg'],
+            'upload_avg': request.POST['upload_avg'],
+            'download_over_limit': request.POST['download_over_limit'],
+            'contract': request.POST['contract']
+        }
 
-        data = [[float(tv_subscriber), float(movie_subscriber), float(subscription_age), float(bill_avg), float(remaining_contract), float(service_failure_count), float(download_avg), float(upload_avg), float(download_over_limit), float(contract)]]
+        subscription_age = float(customer_data['subscription_age'])
+        subscription_age = subscription_age / 12
+        bill_avg = float(customer_data['bill_avg'])
+        bill_avg = bill_avg / 10000
+        remaining_contract = float(customer_data['remaining_contract'])
+        remaining_contract = remaining_contract / 12
+
+        data = [[float(customer_data['is_tv_subscriber']), float(customer_data['is_movie_package_subscriber']), float(subscription_age), float(bill_avg), float(remaining_contract), float(customer_data['service_failure_count']), float(customer_data['download_avg']), float(customer_data['upload_avg']), float(customer_data['download_over_limit']), float(customer_data['contract'])]]
 
         if selected_model == 'logistic_regression':
             scaler = lr_scaler
@@ -95,6 +107,11 @@ def new_data(request):
             probability = naive_bayes_model.predict_proba(input_scaled)[0][1]
 
         if predict == 1:
+            actions = ['Tawarkan layanan tambahan seperti paket hiburan untuk meningkatkan nilai langganan.', 'Dorong perpanjangan kontrak dengan insentif menarik seperti diskon atau bonus.', 'Berikan penghargaan loyalitas seperti cashback, poin, atau hadiah eksklusif.']
+        else:
+            actions = ['Lakukan intervensi proaktif dengan menghubungi pelanggan yang berisiko tinggi untuk mendengar dan menyelesaikan keluhan mereka.', 'Tawarkan paket atau harga yang lebih fleksibel agar sesuai dengan kebutuhan dan kemampuan pelanggan.', 'Luncurkan kampanye retensi personalisasi berisi penawaran khusus untuk menarik pelanggan agar tetap bertahan.']
+        
+        if predict == 1:
             predict = "Churn"
             result = "High Churn Risk."
         else:
@@ -103,7 +120,14 @@ def new_data(request):
 
         probability = round(probability * 100, 2)
 
-        return render(request, 'new_data.html', {'result' : result, 'predict': predict, 'selected_model': selected_model, 'probability' : probability})
+        customer_data['bill_avg'] = int(customer_data['bill_avg'])
+        customer_data['bill_avg'] = locale.format_string('%d', customer_data['bill_avg'], grouping=True)
+        customer_data['subscription_age'] = int(customer_data['subscription_age'])
+        customer_data['remaining_contract'] = int(customer_data['remaining_contract'])
+        customer_data['service_failure_count'] = int(customer_data['service_failure_count'])
+        customer_data['download_over_limit'] = int(customer_data['download_over_limit'])
+
+        return render(request, 'new_data.html', {'result' : result, 'predict': predict, 'selected_model': selected_model, 'probability' : probability, 'customer_data': customer_data, 'actions': actions})
 
     else:
         return render(request, 'new_data.html')
@@ -116,12 +140,12 @@ def existing_data(request):
         customer_data = Customer.objects.get(id=customer_id)
 
         if customer_data.remaining_contract == 0:
-            customer_data_contract = 0
+            customer_data.contract = 0
         else:
-            customer_data_contract = 1
+            customer_data.contract = 1
 
         # data = [[float(customer_data.is_tv_subscriber), float(customer_data.is_movie_package_subscriber), float(customer_data.subscription_age), float(customer_data.bill_avg), float(customer_data.remaining_contract), float(customer_data.service_failure_count), float(customer_data.download_avg), float(customer_data.upload_avg), float(customer_data.download_over_limit)]]
-        data = [[float(customer_data.is_tv_subscriber), float(customer_data.is_movie_package_subscriber), customer_data.subscription_age, customer_data.bill_avg, customer_data.remaining_contract, customer_data.service_failure_count, customer_data.download_avg, customer_data.upload_avg, customer_data.download_over_limit, float(customer_data_contract)]]
+        data = [[float(customer_data.is_tv_subscriber), float(customer_data.is_movie_package_subscriber), customer_data.subscription_age, customer_data.bill_avg, customer_data.remaining_contract, customer_data.service_failure_count, customer_data.download_avg, customer_data.upload_avg, customer_data.download_over_limit, float(customer_data.contract)]]
 
         if selected_model == 'logistic_regression':
             scaler = lr_scaler
@@ -180,6 +204,11 @@ def existing_data(request):
             probability = naive_bayes_model.predict_proba(input_scaled)[0][1]
 
         if predict == 1:
+            actions = ['Tawarkan layanan tambahan seperti paket hiburan untuk meningkatkan nilai langganan.', 'Dorong perpanjangan kontrak dengan insentif menarik seperti diskon atau bonus.', 'Berikan penghargaan loyalitas seperti cashback, poin, atau hadiah eksklusif.']
+        else:
+            actions = ['Lakukan intervensi proaktif dengan menghubungi pelanggan yang berisiko tinggi untuk mendengar dan menyelesaikan keluhan mereka.', 'Tawarkan paket atau harga yang lebih fleksibel agar sesuai dengan kebutuhan dan kemampuan pelanggan.', 'Luncurkan kampanye retensi personalisasi berisi penawaran khusus untuk menarik pelanggan agar tetap bertahan.']
+
+        if predict == 1:
             predict = "Churn"
             result = "High Churn Risk."
         else:
@@ -188,6 +217,16 @@ def existing_data(request):
 
         probability = round(probability * 100, 2)
 
-        return render(request, 'existing_data.html', {'result' : result, 'predict': predict, 'selected_model': selected_model, 'probability' : probability, 'customer_data': customer_data})
+        customer_data.bill_avg = customer_data.bill_avg * 10000
+        customer_data.bill_avg = int(customer_data.bill_avg)
+        customer_data.bill_avg = locale.format_string('%d', customer_data.bill_avg, grouping=True)
+        customer_data.subscription_age = customer_data.subscription_age * 12
+        customer_data.subscription_age = int(customer_data.subscription_age)
+        customer_data.remaining_contract = customer_data.remaining_contract * 12
+        customer_data.remaining_contract = int(customer_data.remaining_contract)
+        customer_data.service_failure_count = int(customer_data.service_failure_count)
+        customer_data.download_over_limit = int(customer_data.download_over_limit)
+
+        return render(request, 'existing_data.html', {'result' : result, 'predict': predict, 'selected_model': selected_model, 'probability' : probability, 'customer_data': customer_data, 'actions': actions})
     else:
         return render(request, 'existing_data.html')
