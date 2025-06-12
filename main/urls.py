b77edd83a9e0ab0fc6b5f421ab9b9b1bb130642b
Vincent Alexander Haris <vincentalexx03@gmail.com>
2025-06-12 22:06:21 +0700

    fix urls

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.new_data, name='new_data'),
-    path('existing_data/', views.existing_data, name='existing_data'),
+    path('', views.existing_data, name='existing_data'),
+    path('new-customer', views.new_data, name='new_data'),
 ]
